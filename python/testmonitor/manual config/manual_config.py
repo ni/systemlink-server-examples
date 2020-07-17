83029132380f2c6926837c4a35ca76471d5b7c0e
Matthew Vaterlaus <matthew.vaterlaus@ni.com>
2020-07-17 13:01:55 -0500

    Add module documentation.

--- python/testmonitor/manual config/manual_config.py
+++ python/testmonitor/manual config/manual_config.py
@@ -1,6 +1,16 @@
 """
 SystemLink Test Monitor manual config example
 
+This is an example of uploading a test result to the SystemLink Test Monitor service
+using a manual configuration.  This is helpful when accessing the SystemLink server from
+a device that is not registered with the server.
+
+This example accepts command-line arguments to configure the connection to the SystemLink server.
+The connection URL can be specified to connect to a specific SystemLink server.  Optionally,
+the user can specify either an API key or a username and password to connect to the server.
+
+Once a connection is configured, a simple test result (with no child steps) is created on
+the server to validate that the connection information succeeds in accessing the server.
 """
 
 import sys
